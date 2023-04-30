b2718123b4e6adc8fcbea9c7664b7c0a573600ec
Marcelo Fabiano <marcelofabianov@gmail.com>
2023-04-29 23:03:38 -0300

    chore: create interface ICreateCustomerUseCase

--- src/customer/use-cases/create-customer.use-case.ts
+++ src/customer/use-cases/create-customer.use-case.ts
@@ -3,7 +3,11 @@ import { CustomerRepository } from '@customer/use-cases/customer.repository';
 import { createCustomerDto } from '@customer/create-customer.dto';
 import { Customer } from '@customer/customer.entity';
 
-export class CreateCustomerUseCase {
+export interface ICreateCustomerUseCase {
+  execute(dto: z.infer<typeof createCustomerDto>): Promise<Customer>;
+}
+
+export class CreateCustomerUseCase implements ICreateCustomerUseCase {
   constructor(
     private readonly customerRepository: CustomerRepository
   ) { }
